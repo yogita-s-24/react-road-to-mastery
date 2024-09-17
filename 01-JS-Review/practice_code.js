31577171990eefab8b5c6cee5c726d8c6866768c
Yogita Shete <yogitashete24@gmail.com>
2024-09-17 20:16:18 +0530

    Add function to find uncommon words from two sentences

---
+++ 01-JS-Review/practice_code.js
@@ -0,0 +1,27 @@
+var uncommonFromSentences = function (s1, s2) {
+  const word1 = s1.split(" ");
+  const word2 = s2.split(" ");
+
+  const combined_list = word1.concat(word2);
+
+  console.log(combined_list);
+
+  let output = [];
+  let repeated_words = [];
+
+  for (let word of combined_list) {
+    if (!output.includes(word)) {
+      output.push(word);
+    } else {
+      repeated_words.push(word);
+    }
+  }
+
+  for (let word of repeated_words) {
+    if (output.includes(word)) {
+      output = output.filter((item) => item !== word);
+    }
+  }
+
+  return output;
+};
