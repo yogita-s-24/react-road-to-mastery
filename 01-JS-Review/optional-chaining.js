4e3c13f83fb64d7aaf1e11cc56c2fd979db13360
Yogita Shete <yogitashete24@gmail.com>
2024-09-19 11:30:07 +0530

    Implement optional chaining for safe property access in object logging

---
+++ 01-JS-Review/optional-chaining.js
@@ -0,0 +1,14 @@
+// Optional chaining is used primarily to simplify and safely access properties or methods of an object when you are unsure if certain intermediate properties exist. It helps you avoid errors and makes the code more concise and readable.
+
+// optional chaining is used to avoid tedious checks for null or undefined values, improve the reliability of your code, and keep it concise and readable.
+
+object = {
+    name : "Yogita",
+    age : "23",
+    adress : "Pune",
+    Education : "Msc Computer Science",
+}
+
+// console.log(object);
+console.log(object?.names);
+
