66c47ff8c0adf8333e54f12c76ea0a04171fba70
Yogita Shete <yogitashete24@gmail.com>
2024-09-26 12:29:27 +0530

    Add toggle functionality to show/hide step navigation and manage visibility state

--- 03-step/src/App.jsx
+++ 03-step/src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import { useState } from "react";
 
 const messages = [
   "Learn React ⚛️",
@@ -7,41 +7,55 @@ const messages = [
 ];
 
 export default function App() {
-
   const [step, setStep] = useState(1);
+  const [isOpen, setisOpen] = useState(true);
+  // const [test, setTest] = useState({name : "Yogtia"});
 
-  const handlePrevious = () =>{
-    if(step > 1) setStep(step - 1);
-  }
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
 
-  const handleNext = () =>{
-    if(step < 3) setStep(step + 1);
-  }
-  
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+    // setTest({name : "Yogtia"});
+  };
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <div>
+      <button
+        className="close"
+        onClick={() => {
+          setisOpen(!isOpen);
+        }}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step} : {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
 
-      <div className="buttons">
-        <button 
-        style={{ background: "#7950f2", color: "#fff" }}
-        onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button 
-        style={{ background: "#7950f2", color: "#fff" }}
-        onClick={handleNext}>
-        Next</button>
-      </div>
+          <div className="buttons">
+            <button
+              style={{ background: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}>
+              Previous
+            </button>
+            <button
+              style={{ background: "#7950f2", color: "#fff" }}
+              onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
