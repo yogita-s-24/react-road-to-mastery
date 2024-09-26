32f1f76563dc9a0895ad115332ca6164a6dd2c7f
Yogita Shete <yogitashete24@gmail.com>
2024-09-26 11:49:10 +0530

    Add step navigation with state management using useState for dynamic message display

--- 03-step/src/App.jsx
+++ 03-step/src/App.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react"
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,20 +7,23 @@ const messages = [
 ];
 
 export default function App() {
+
+  const [step, setStep] = useState(1);
+
   const handlePrevious = () =>{
-    alert("Previous");
+    if(step > 1) setStep(step - 1);
   }
 
   const handleNext = () =>{
-    alert("Next");
+    if(step < 3) setStep(step + 1);
   }
-  const step = 3;
+  
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
