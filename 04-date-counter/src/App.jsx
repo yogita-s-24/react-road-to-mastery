1746269642b2a119ef9e35bae62f4cef9527c9fc
Yogita Shete <yogitashete24@gmail.com>
2024-10-09 22:55:57 +0530

    add usestate functionality

--- 04-date-counter/src/App.jsx
+++ 04-date-counter/src/App.jsx
@@ -1,9 +1,7 @@
-// import { useState } from 'react';
+import { useState } from "react";
 import "./index.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <Counter />
@@ -12,31 +10,44 @@ function App() {
 }
 
 function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(0);
+
   return (
     <div className="border border-black w-96 mx-auto mt-10 p-10 rounded-lg bg-[rgb(57,56,61)]">
       <div className="text-center font-semibold text-2xl text-white">Test</div>
       <div className="text-center mt-5">
         <div className="flex justify-center">
           <div className="flex flex-row gap-16 items-center">
-            <div className="bg-black text-white w-12 h-12 flex items-center justify-center rounded-full font-extrabold cursor-pointer">
+            <button
+              className="bg-black text-white w-12 h-12 flex items-center justify-center rounded-full font-extrabold cursor-pointer"
+              onClick={() => setStep((c) => c - 1)}>
               ➖
-            </div>
-            <h3 className="text-white font-semibold">Step : 1</h3>
-            <div className="bg-black text-white w-12 h-12 flex items-center justify-center rounded-full font-extrabold cursor-pointer">
+            </button>
+            <h3 className="text-white font-semibold">Step : {step}</h3>
+            <button
+              className="bg-black text-white w-12 h-12 flex items-center justify-center rounded-full font-extrabold cursor-pointer"
+              onClick={() => setStep((c) => c + 1)}>
               ➕
-            </div>
+            </button>
           </div>
         </div>
 
         <div className="flex justify-center mt-10">
           <div className="flex flex-row gap-16 items-center">
-            <div className="bg-black text-white w-12 h-12 flex items-center justify-center rounded-full font-extrabold cursor-pointer">
+            <button 
+            className="bg-black text-white w-12 h-12 flex items-center justify-center rounded-full font-extrabold cursor-pointer"
+            onClick={() => setCount((c) => c - step)}
+            >
               ➖
-            </div>
-            <h3 className="text-white font-semibold">Count : 1</h3>
-            <div className="bg-black text-white w-12 h-12 flex items-center justify-center rounded-full font-extrabold cursor-pointer">
+            </button>
+            <h3 className="text-white font-semibold">Count : {count}</h3>
+            <button 
+            className="bg-black text-white w-12 h-12 flex items-center justify-center rounded-full font-extrabold cursor-pointer"
+            onClick={() => setCount((c) => c + step)}
+            >
               ➕
-            </div>
+            </button>
           </div>
         </div>
       </div>
