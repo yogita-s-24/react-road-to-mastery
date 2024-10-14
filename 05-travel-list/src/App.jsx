952a256cac9be0e01398c6be35f8ba95533be76f
Yogita Shete <yogitashete24@gmail.com>
2024-10-14 22:25:21 +0530

    add form handling

---
+++ 05-travel-list/src/App.jsx
@@ -0,0 +1,90 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false,
+  },
+  {
+    id: 2,
+    description: "Socks",
+    quantity: 12,
+    packed: true,
+  },
+  {
+    id: 3,
+    description: "Charger",
+    quantity: 1,
+    packed: false,
+  },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away</h1>;
+}
+
+function Form() {
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(e)
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need your trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have x items on your list, and you already packed x (x%)</em>
+    </footer>
+  );
+}
